ba8de9ebcf3ecebd82cb8114a3b7a92db6074760
Ato Araki <ato.araki@gmail.com>
2014-08-15 14:39:15 +0900

    ch02/ex08: add test

---
+++ src/test/java/com/github/atotto/java8horstmann/ch02/ex08/ZipTest.java
@@ -0,0 +1,38 @@
+package com.github.atotto.java8horstmann.ch02.ex08;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Iterator;
+import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
+
+import org.junit.Test;
+
+public class ZipTest {
+
+	public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+		Builder<T> builder = Stream.builder();
+
+		Iterator<T> it1 = first.iterator();
+		Iterator<T> it2 = second.iterator();
+
+		while (it1.hasNext() && it2.hasNext()) {
+			builder.add(it1.next());
+			builder.add(it2.next());
+		}
+
+		return builder.build();
+	}
+
+	@Test
+	public void test() {
+		Stream<String> st1 = Stream.of(new String[] { "s", "re", "!!" });
+		Stream<String> st2 = Stream.of(new String[] { "t", "am" });
+
+		Stream<String> zipped = zip(st1, st2);
+		String message = zipped.reduce("", (x, y) -> x + y);
+
+		assertThat("stream", is(message));
+	}
+}
