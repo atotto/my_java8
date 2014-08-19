368857005079c1c7dbb489900f594f1f9da6e516
Ato Araki <ato.araki@gmail.com>
2014-08-19 10:50:11 +0900

    refactor
    use AtomicLong

--- src/test/java/com/github/atotto/java8horstmann/ch02/ex10/AverageTest.java
+++ src/test/java/com/github/atotto/java8horstmann/ch02/ex10/AverageTest.java
@@ -3,6 +3,7 @@ package com.github.atotto.java8horstmann.ch02.ex10;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
@@ -11,11 +12,13 @@ import org.junit.Test;
 public class AverageTest {
 
 	public static Double bad_average(Stream<Double> stream) {
+		AtomicLong count = new AtomicLong(0);
 		Double sum = stream.reduce(0.0, (val1, val2) -> {
+			count.getAndIncrement();
 			return val1 + val2;
 		});
-		// cannot obtain count from used stream.
-		return sum / 4;
+		// cannot obtain count() from used stream.
+		return sum / count.get();
 	}
 
 	public static Double average(Stream<Double> stream) {
@@ -23,7 +26,15 @@ public class AverageTest {
 	}
 
 	@Test
-	public void test() {
+	public void testBadAverage() {
+		DoubleStream stream = DoubleStream.of(10.0, 0.2, 0.4, 2.4);
+		Double ave = bad_average(stream.boxed());
+
+		assertThat(ave, is((10.0 + 0.2 + 0.4 + 2.4) / 4));
+	}
+
+	@Test
+	public void testAverage() {
 		DoubleStream stream = DoubleStream.of(10.0, 0.2, 0.4, 2.4);
 		Double ave = average(stream.boxed());
 
