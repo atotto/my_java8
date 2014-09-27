70e122dd1795d2e5790bd4268ba25633bf4366c6
Ato Araki <ato.araki@gmail.com>
2014-09-27 16:21:13 +0900

    ch03/ex12: add LatentImage class

---
+++ src/test/java/com/github/atotto/java8horstmann/ch03/ex12/LatentImage.java
@@ -0,0 +1,41 @@
+package com.github.atotto.java8horstmann.ch03.ex12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+public class LatentImage {
+	private Image in;
+	private List<UnaryOperator<Color>> pendingOperations;
+
+	public static LatentImage from(Image in) {
+		LatentImage result = new LatentImage();
+		result.in = in;
+		result.pendingOperations = new ArrayList<>();
+		return result;
+	}
+
+	LatentImage transform(UnaryOperator<Color> f) {
+		pendingOperations.add(f);
+		return this;
+	}
+
+	public Image toImage() {
+		int width = (int) in.getWidth();
+		int height = (int) in.getHeight();
+		WritableImage out = new WritableImage(width, height);
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				Color c = in.getPixelReader().getColor(x, y);
+				for (UnaryOperator<Color> f : pendingOperations)
+					c = f.apply(c);
+				out.getPixelWriter().setColor(x, y, c);
+			}
+		}
+		return out;
+	}
+}
