d5cfc4dcc530b8a12945032ed15670f992424fd7
Ato Araki <ato.araki@gmail.com>
2014-10-01 22:50:30 +0900

    ex03/ex17: add doInParallelAsync

---
+++ src/test/java/com/github/atotto/java8horstmann/ch03/ex17/ExceptionTest.java
@@ -0,0 +1,128 @@
+package com.github.atotto.java8horstmann.ch03.ex17;
+
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+import org.junit.Test;
+
+public class ExceptionTest {
+
+	public static void doInParallelAsync(Runnable first, Runnable second,
+			Consumer<Throwable> handler) {
+		Thread t1 = new Thread() {
+			public void run() {
+				try {
+					first.run();
+				} catch (Throwable t) {
+					handler.accept(t);
+				}
+			}
+		};
+		Thread t2 = new Thread() {
+			public void run() {
+				try {
+					second.run();
+				} catch (Throwable t) {
+					handler.accept(t);
+				}
+			}
+		};
+		t1.start();
+		t2.start();
+	}
+
+	@Test
+	public void testDoInParallelAsync_no_exception()
+			throws InterruptedException {
+		AtomicReference<String> first_message = new AtomicReference<>();
+		AtomicReference<String> second_message = new AtomicReference<>();
+		AtomicReference<String> exception_message = new AtomicReference<>();
+		CountDownLatch latch = new CountDownLatch(2);
+
+		doInParallelAsync(() -> {
+			first_message.set("first");
+			latch.countDown();
+		}, () -> {
+			second_message.set("second");
+			latch.countDown();
+		}, (e) -> {
+			exception_message.set(e.getMessage());
+		});
+
+		latch.await();
+		assertThat(first_message.get(), is("first"));
+		assertThat(second_message.get(), is("second"));
+		assertThat(exception_message.get(), is(nullValue()));
+	}
+
+	@Test
+	public void testDoInParallelAsync_get_exception_from_first()
+			throws InterruptedException {
+		AtomicReference<String> second_message = new AtomicReference<>();
+		AtomicReference<String> exception_message = new AtomicReference<>();
+		CountDownLatch latch = new CountDownLatch(2);
+
+		doInParallelAsync(() -> {
+			throw new IllegalStateException("first");
+		}, () -> {
+			second_message.set("second");
+			latch.countDown();
+		}, (e) -> {
+			exception_message.set(e.getMessage());
+			latch.countDown();
+		});
+
+		latch.await();
+		assertThat(second_message.get(), is("second"));
+		assertThat(exception_message.get(), is("first"));
+	}
+
+	@Test
+	public void testDoInParallelAsync_get_exception_from_second()
+			throws InterruptedException {
+		AtomicReference<String> first_message = new AtomicReference<>();
+		AtomicReference<String> exception_message = new AtomicReference<>();
+		CountDownLatch latch = new CountDownLatch(2);
+
+		doInParallelAsync(() -> {
+			first_message.set("first");
+			latch.countDown();
+		}, () -> {
+			throw new IllegalStateException("second");
+		}, (e) -> {
+			exception_message.set(e.getMessage());
+			latch.countDown();
+		});
+
+		latch.await();
+		assertThat(first_message.get(), is("first"));
+		assertThat(exception_message.get(), is("second"));
+	}
+
+	@Test
+	public void testDoInParallelAsync_get_exception_from_both()
+			throws InterruptedException {
+		AtomicReference<String> exception_message = new AtomicReference<>();
+		CountDownLatch latch = new CountDownLatch(2);
+
+		doInParallelAsync(() -> {
+			throw new IllegalStateException("first");
+		}, () -> {
+			throw new IllegalStateException("second");
+		}, (e) -> {
+			latch.countDown();
+			exception_message.set(e.getMessage());
+		});
+
+		latch.await();
+		assertThat(exception_message.get(),
+				anyOf(equalTo("first"), equalTo("second")));
+	}
+}
