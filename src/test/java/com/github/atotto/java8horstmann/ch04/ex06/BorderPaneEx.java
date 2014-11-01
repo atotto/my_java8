6aebc0f8e1538b86a1d4b5995a4fa96cfcf12256
Ato Araki <ato.araki@gmail.com>
2014-11-01 09:58:10 +0900

    ch04/ex06: update solution

--- src/test/java/com/github/atotto/java8horstmann/ch04/ex06/BorderPaneEx.java
+++ src/test/java/com/github/atotto/java8horstmann/ch04/ex06/BorderPaneEx.java
@@ -1,6 +1,7 @@
 package com.github.atotto.java8horstmann.ch04.ex06;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -10,10 +11,12 @@ public class BorderPaneEx extends Application {
 	public void start(Stage stage) {
 		BorderPane pane = new BorderPane();
 		pane.setTop(new Button("Top"));
+		BorderPane.setAlignment(pane.getTop(), Pos.TOP_CENTER);
 		pane.setLeft(new Button("Left"));
 		pane.setCenter(new Button("Center"));
 		pane.setRight(new Button("Right"));
 		pane.setBottom(new Button("Bottom"));
+		BorderPane.setAlignment(pane.getBottom(), Pos.BOTTOM_CENTER);
 		stage.setScene(new Scene(pane));
 		stage.show();
 	}
