c7ae4314998d6b057198e46f4a1918d1a23d6d93
Ato Araki <ato.araki@gmail.com>
2015-01-15 21:19:06 +0900

    ch07: update progress report

--- src/test/java/com/github/atotto/java8horstmann/ProgressReportTest.java
+++ src/test/java/com/github/atotto/java8horstmann/ProgressReportTest.java
@@ -22,7 +22,7 @@ public class ProgressReportTest {
 		chapter.put("ch04", new C(1, 10));
 		chapter.put("ch05", new C(1, 12));
 		chapter.put("ch06", new C(1, 11));
-		// chapter.put("ch07", new C(1, 10));
+		chapter.put("ch07", new C(1, 10));
 		// chapter.put("ch08", new C(1, 16));
 		// chapter.put("ch09", new C(1, 12));
 		// TODO: add next chapter exercises.
